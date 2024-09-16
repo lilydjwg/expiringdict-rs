8e5fe37dc4b20935b53ea6ac40bd698dc6a939c9
lilydjwg <lilydjwg@gmail.com>
2024-09-16 17:24:45 +0800

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! A dict / HashMap whose items expire over time
+//!
+//! This is a simple wrapper over [`HashMap`] and when you call its [`expire()`](ExpiringDict::expire) method, items
+//! inserted early expire and disappear.
+//!
+//! There is no external dependency like tokio or the like, but you need to call [`expire()`](ExpiringDict::expire)
+//! at appropriate time.
 use std::collections::HashMap;
 use std::time::{SystemTime, Instant, Duration};
 use std::borrow::Borrow;
@@ -22,6 +29,10 @@ where T: Add<Duration, Output=T> + PartialOrd
     }
   }
 
+  /// Get an item from the dict.
+  ///
+  /// Remember to call [`expire()`](Self::expire) before calling this method unless you don't care
+  /// expired items being returned.
   pub fn get<Q>(&self, k: &Q) -> Option<&V>
     where K: Borrow<Q>,
           Q: Hash + Eq + ?Sized
@@ -37,6 +48,9 @@ where T: Add<Duration, Output=T> + PartialOrd
     self.container.insert(k, (v, T::now() + ttl)).map(|(item, _)| item)
   }
 
+  /// Remove expired items.
+  ///
+  /// Remember to call this method before use!
   pub fn expire(&mut self) {
     let now = T::now();
     self.container.retain(|_k, (_v, t)| *t > now);
